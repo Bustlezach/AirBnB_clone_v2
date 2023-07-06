9df0452b7b85c63d42d1eee4d6634750f0f82f42
root <bustlezach001@gmail.com>
2023-07-05 17:34:09 -0700

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""Compress before sending"""
-from fabric.api import *
-from datetime import datetime
-import os.path
+from fabric.api import local
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """Generates a .tgz archive"""
-   
-   
-    # Create the file name using the current date and time
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
-    
+    """ Generate a .tgz archive"""
 
+    fname = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(fname))
+
+        return "versions/web_static_{}.tgz".format(fname)
+
+    except Exception as e:
+        return None
