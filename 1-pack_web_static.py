ed573a8cf3a5f041617b81b5abfe3c02760359e7
bustlezach <bustlezach001@gmail.com>
2023-07-08 06:37:28 -0700

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,16 @@
 from fabric.api import local
-from datetime import datetime
+from time import strftime
 
 def do_pack():
     """Generate a .tgz archive"""
 
-    # Generate the archive path
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    fname = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        archive_path = "versions/web_static_{}.tgz".format(now)
-        result = local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(fname))
+
+        return "versions/web_static_{}.tgz".format(fname)
 
     except Exception as e:
         return None
