a21e38cf701bda58a8af1640cc1646e88022f36c
bustlezach <bustlezach001@gmail.com>
2023-07-08 07:10:20 -0700

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Make achive file"""
-
-from fabric.api import local
+"""
+Make achive file
+"""
+from fabric.api import local, env
 from datetime import datetime
 
 env.user = 'ubuntu'
 env.hosts = ['52.205.172.113', '54.160.64.104']
 
+
 def do_pack():
-    """Generate a .tgz archive"""
-
-    # Generate the archive path
+    """
+    Generate a .tgz archive
+    """
     now = datetime.now().strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        archive_path = "versions/web_static_{}.tgz".format(now)
-        result = local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-
-    except Exception as e:
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
         return None
-
