ec48c70c43b20b8014aad6725bc071f19cdd0b2b
bustlezach <bustlezach001@gmail.com>
2023-07-22 12:42:49 -0700

    task

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,10 @@
--- Creates a MySQL server having:
---   Database hbnb_dev_db.
-
+-- A script that prepares a MySQL server:
+--  Creates hbnb_dev_db database
+--  Set password to hbnb_dev_pwd
+--  hbnb_dev should have all privileges on the database hbnb_dev_db
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
